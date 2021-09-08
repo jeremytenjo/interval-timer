153800fa8e346017b1f6c38a4e05c54bf0372a86
Jeremy Tenjo <tenjojeremy@gmail.com>
2021-09-07 23:52:19 -0400

    extracted ui

--- src/lib/components/layouts/ShellLayout/AppBar/containers/Items/VolumeControl/index.js
+++ src/lib/components/layouts/ShellLayout/AppBar/containers/Items/VolumeControl/index.js
@@ -1,79 +1,18 @@
-import VolumeIcon from '../../../../../../icons/Volume'
-import IconButton from '../../../../../../IconButton'
-import Popover from '../../../../../../Popover'
-import Slider from '../../../../../../Slider'
-import VolumeDown from '../../../../../../icons/VolumeDown'
-import VolumeUp from '../../../../../../icons/VolumeUp'
-import Box from '../../../../../../Box'
+import VolumeControlUi from './ui'
 
-import * as styles from './styles'
-
+// hanlde phone volume
 export default function VolumeControl() {
-  const [anchorEl, setAnchorEl] = useState(null)
-  const [volumeAmount, setVolumeAmount] = useState(50)
+  const updateVolume = (newValue) => {}
 
-  const handleClick = (event) => {
-    setAnchorEl(event.currentTarget)
-  }
+  const mute = () => {}
 
-  const handleClose = () => {
-    setAnchorEl(null)
-  }
-
-  const onVolumeChange = (event, newValue) => {
-    setVolumeAmount(newValue)
-  }
-
-  const mute = () => {
-    setVolumeAmount(0)
-  }
-
-  const setToMaxVolume = () => {
-    setVolumeAmount(100)
-  }
-
-  const open = Boolean(anchorEl)
-  const id = open ? 'AppBar_VolumeControl' : undefined
+  const setToMaxVolume = () => {}
 
   return (
-    <>
-      <IconButton onClick={handleClick}>
-        <VolumeIcon />
-      </IconButton>
-
-      <Popover
-        anchorOrigin={{
-          vertical: 'bottom',
-          horizontal: 'right',
-        }}
-        transformOrigin={{
-          vertical: 'top',
-          horizontal: 'center',
-        }}
-        id={id}
-        open={open}
-        anchorEl={anchorEl}
-        onClose={handleClose}
-      >
-        <Box sx={styles.sliderWrapper}>
-          <Box sx={styles.sliderWrapperInner}>
-            <IconButton onClick={mute}>
-              <VolumeDown sx={{ stroke: 'white' }} />
-            </IconButton>
-
-            <Slider
-              sx={styles.slider}
-              aria-label='Volume'
-              value={volumeAmount}
-              onChange={onVolumeChange}
-            />
-
-            <IconButton onClick={setToMaxVolume}>
-              <VolumeUp sx={{ stroke: 'white' }} />
-            </IconButton>
-          </Box>
-        </Box>
-      </Popover>
-    </>
+    <VolumeControlUi
+      onVolumeChange={updateVolume}
+      onMute={mute}
+      onSetToMaxVolume={setToMaxVolume}
+    />
   )
 }
---
+++ src/lib/components/layouts/ShellLayout/AppBar/containers/Items/VolumeControl/ui.js
@@ -0,0 +1,86 @@
+import VolumeIcon from '../../../../../../icons/Volume'
+import IconButton from '../../../../../../IconButton'
+import Popover from '../../../../../../Popover'
+import Slider from '../../../../../../Slider'
+import VolumeDown from '../../../../../../icons/VolumeDown'
+import VolumeUp from '../../../../../../icons/VolumeUp'
+import Box from '../../../../../../Box'
+
+import * as styles from './styles'
+
+export default function VolumeControlUi({
+  onMute = () => null,
+  onSetToMaxVolume = () => null,
+  onVolumeChange = () => null,
+}) {
+  const [anchorEl, setAnchorEl] = useState(null)
+  const [volumeAmount, setVolumeAmount] = useState(50)
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget)
+  }
+
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
+
+  const updateVolume = (event, newValue) => {
+    setVolumeAmount(newValue)
+    onVolumeChange(newValue)
+  }
+
+  const mute = () => {
+    setVolumeAmount(0)
+    onMute()
+  }
+
+  const setToMaxVolume = () => {
+    setVolumeAmount(100)
+    onSetToMaxVolume()
+  }
+
+  const open = Boolean(anchorEl)
+  const id = open ? 'AppBar_VolumeControl' : undefined
+
+  return (
+    <>
+      <IconButton onClick={handleClick}>
+        <VolumeIcon />
+      </IconButton>
+
+      <Popover
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'right',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'center',
+        }}
+        id={id}
+        open={open}
+        anchorEl={anchorEl}
+        onClose={handleClose}
+      >
+        <Box sx={styles.sliderWrapper}>
+          <Box sx={styles.sliderWrapperInner}>
+            <IconButton onClick={mute}>
+              <VolumeDown sx={{ stroke: 'white' }} />
+            </IconButton>
+
+            <Slider
+              sx={styles.slider}
+              aria-label='Volume'
+              value={volumeAmount}
+              onChange={updateVolume}
+            />
+
+            <IconButton onClick={setToMaxVolume}>
+              <VolumeUp sx={{ stroke: 'white' }} />
+            </IconButton>
+          </Box>
+        </Box>
+      </Popover>
+    </>
+  )
+}
