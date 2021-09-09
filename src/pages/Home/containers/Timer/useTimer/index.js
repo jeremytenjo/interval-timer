ffe7400401ebdac703135fd828fc7d982263acd8
Jeremy Tenjo <tenjojeremy@gmail.com>
2021-09-09 15:34:35 -0400

    clean

--- src/pages/Home/containers/Timer/useTimer/index.js
+++ src/pages/Home/containers/Timer/useTimer/index.js
@@ -9,21 +9,20 @@ export default function useTimer({
   //   console.log({ initialRepetitions, initialSets, initialWorkoutTime, initialRestTime })
   const timerControls = useTimerControls()
   const [type, setType] = useState('Workout')
-  const isRest = type === 'Rest'
 
+  const isRest = type === 'Rest'
   const repetitions = initialRepetitions
   const sets = initialSets
   const workoutTime = initialWorkoutTime
   const restTime = initialRestTime
+  const duration = isRest ? initialRestTime : initialWorkoutTime
+  const color = isRest ? '#D72E33' : '#36B273'
 
   const startNextRepetition = () => {
     // if has more reps
     setType((currentStep) => (currentStep === 'Rest' ? 'Workout' : 'Rest'))
     timerControls.restartTimer()
   }
-
-  const duration = isRest ? initialRestTime : initialWorkoutTime
-  const color = isRest ? '#D72E33' : '#36B273'
 
   return {
     repetitions,
