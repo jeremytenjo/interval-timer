7e4d5f9da61f870296e0e1cbdf3aa7801add0a32
Jeremy Tenjo <tenjojeremy@gmail.com>
2021-09-13 22:01:20 -0400

    removed use elsaptged time hook

--- src/data/useElapsedTime/index.js
+++
@@ -1,9 +0,0 @@
-import create from 'zustand'
-
-const useElapsedTime = create((set) => ({
-  elapsedTime: 0,
-
-  setElapsedTime: ({ newValue }) => set(() => ({ elapsedTime: newValue })),
-}))
-
-export default useElapsedTime
