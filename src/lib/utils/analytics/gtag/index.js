9d069cf14aec29e46926450e5f8577d2011cf74a
Jeremy Tenjo <tenjojeremy@gmail.com>
2021-09-25 22:43:22 -0400

    comment

--- src/lib/utils/analytics/gtag/index.js
+++ src/lib/utils/analytics/gtag/index.js
@@ -2,6 +2,8 @@
 
 /**
  * [Google Docs Dashboard](https://analytics.google.com/analytics/web/?hl=en_US#/p281545046/reports/reportinghub)
+ *
+ * [Docs](https://developers.google.com/analytics/devguides/collection/gtagjs)
  */
 export default function gtag() {
   return gtag
