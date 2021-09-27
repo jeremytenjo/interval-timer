6d599b7b40de9d0ef4854f7db4f8fdd16b1bde46
Jeremy Tenjo <tenjojeremy@gmail.com>
2021-09-27 16:47:14 -0400

    comment

--- src/lib/components/CountdownCircleTimer/index.js
+++ src/lib/components/CountdownCircleTimer/index.js
@@ -1,3 +1,4 @@
+// https://github.com/vydimitrov/react-countdown-circle-timer/tree/master/packages/web#readme
 import { CountdownCircleTimer } from 'react-countdown-circle-timer'
 
 export default CountdownCircleTimer
