d3111c74648b06b9058ecca4f24d58f8884aacfb
Jeremy Tenjo <tenjojeremy@gmail.com>
2021-10-03 10:57:23 -0400

    showerror

--- src/globalState/useSettings/handlers/useSaveSettings/index.js
+++ src/globalState/useSettings/handlers/useSaveSettings/index.js
@@ -2,6 +2,7 @@ import useAsync from '@useweb/use-async'
 import useOnTrue from '@useweb/use-on-true'
 
 import useSnackbar from '../../../../lib/components/Snackbar/useSnackbar'
+import useShowError from '../../../../lib/components/feedback/useShowError'
 
 export default function useSaveSettings() {
   const snackbar = useSnackbar()
@@ -11,6 +12,8 @@ export default function useSaveSettings() {
   }
 
   const saveSettings = useAsync(fetcher)
+
+  useShowError(saveSettings.error, 'Error saving settings, please try again')
 
   useOnTrue(saveSettings.result, () => {
     snackbar.show({ message: 'Settings saved' })
