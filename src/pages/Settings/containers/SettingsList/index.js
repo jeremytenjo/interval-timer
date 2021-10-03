2876d31a0176274d804f9e409039773815edce59
Jeremy Tenjo <tenjojeremy@gmail.com>
2021-10-03 09:22:37 -0400

    settings pga

--- src/pages/Settings/containers/SettingsList/index.js
+++ src/pages/Settings/containers/SettingsList/index.js
@@ -1,36 +1,12 @@
 import Box from 'lib/components/Box'
-import Accordion from '@mui/material/Accordion'
-import AccordionSummary from '@mui/material/AccordionSummary'
-import AccordionDetails from '@mui/material/AccordionDetails'
-import Typography from '@mui/material/Typography'
 
 import * as styles from './styles.js'
+import SoundSettings from './SoundSettings'
 
 export default function SettingsList() {
   return (
     <Box sx={styles.wrapper}>
-      <Accordion>
-        <AccordionSummary aria-controls='panel1a-content' id='panel1a-header'>
-          <Typography>Sound</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada
-            lacus ex, sit amet blandit leo lobortis eget.
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
-      <Accordion>
-        <AccordionSummary aria-controls='panel2a-content' id='panel2a-header'>
-          <Typography>Accordion 2</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada
-            lacus ex, sit amet blandit leo lobortis eget.
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
+      <SoundSettings />
     </Box>
   )
 }
