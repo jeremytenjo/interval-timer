053b83fd02aac71fbb833b1e830421a5dd3e2e3f
Jeremy Tenjo <tenjojeremy@gmail.com>
2021-10-06 14:39:35 -0400

    settings data

--- src/data/settings/useSettings/index.js
+++ src/data/settings/useSettings/index.js
@@ -1,32 +1,9 @@
-import create from 'zustand'
-
-import useAuth from '../../../globalState/useAuth'
+import useCollection from '../../../lib/utils/data/useCollection'
 
 import defaultSettings from './defaultSettings'
-import useSaveSettings from './handlers/useSaveSettings'
-import useGetSettings from './handlers/useGetSettings'
-import useUpdateSetting from './handlers/useUpdateSetting'
-
-const useSettingsStore = create((set) => ({
-  settings: defaultSettings,
-  setSettings: (newValue) => set(() => ({ settings: newValue })),
-}))
 
 export default function useSettings() {
-  const settingsStore = useSettingsStore()
-  const auth = useAuth()
+  const collection = useCollection('settings')
 
-  const payload = {
-    settingsStore,
-    userId: auth?.user?.uid,
-  }
-
-  useGetSettings(payload)
-  const saveSettings = useSaveSettings(payload)
-  const updateSetting = useUpdateSetting(payload)
-
-  return {
-    saveSettings,
-    updateSetting,
-  }
+  return collection
 }
