e47567e0355f9fced085846d8c75d8e85dd948e6
Jeremy Tenjo <tenjojeremy@gmail.com>
2021-10-06 14:39:48 -0400

    h

--- src/data/settings/useSettings/index.js
+++ src/data/settings/useSettings/index.js
@@ -3,6 +3,7 @@ import useCollection from '../../../lib/utils/data/useCollection'
 import defaultSettings from './defaultSettings'
 
 export default function useSettings() {
+  // TODO
   const collection = useCollection('settings')
 
   return collection
