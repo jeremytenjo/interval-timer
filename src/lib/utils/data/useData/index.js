b1d717377dfcb53afe0c7dfe067bb427bbc7b1d7
Jeremy Tenjo <tenjojeremy@gmail.com>
2021-10-06 10:59:21 -0400

    c

--- src/lib/utils/data/useData/index.js
+++ src/lib/utils/data/useData/index.js
@@ -5,10 +5,9 @@ import useShowError from '../../../components/feedback/useShowError'
 import useLocalStorage from '../../storage/useLocalStorage'
 
 export default function useData({ key, fetcher }) {
+  const dataFetch = useSWRImmutable(key, fetcher)
   const getLocalStorageData = useLocalStorage({ action: 'get', key })
   const setLocalStorageData = useLocalStorage({ action: 'set', key })
-
-  const dataFetch = useSWRImmutable(key, fetcher)
 
   useOnTrue(!dataFetch.data, () => {
     getLocalStorageData.exec()
