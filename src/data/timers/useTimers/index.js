546a9fdfbd4abfa39711ec540024398c4141fab8
Jeremy Tenjo <tenjojeremy@gmail.com>
2021-10-06 20:34:11 -0400

    used onRemove in useTimers

--- src/data/timers/useTimers/index.js
+++ src/data/timers/useTimers/index.js
@@ -1,24 +1,22 @@
-import useOnTrue from '@useweb/use-on-true'
-
 import useCollection from '../../../lib/utils/data/useCollection'
 import useTimer from '../../../globalState/useTimer'
 
 export default function useTimers() {
   const navigate = useNavigate()
   const timer = useTimer()
-  const collection = useCollection('timers')
+  const collection = useCollection('timers', {
+    onRemove: (result) => {
+      const remainingTimers = result.remainingItems.filter(
+        (item) => item.id !== result.removedItemId,
+      )
 
-  useOnTrue(collection.remove.result, () => {
-    const remainingTimers = collection.get.data.filter(
-      (item) => item.id !== collection.remove.result.removedItemId,
-    )
-
-    if (remainingTimers.length) {
-      timer.setSelectedTimer(remainingTimers[0])
-    } else {
-      timer.setSelectedTimer(false)
-      navigate('/create-timer')
-    }
+      if (remainingTimers.length) {
+        timer.setSelectedTimer(remainingTimers[0])
+      } else {
+        timer.setSelectedTimer(false)
+        navigate('/create-timer')
+      }
+    },
   })
 
   return collection
