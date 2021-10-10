56239f12f02fdea4c84744e5d7b1d7031eacfdfe
Jeremy Tenjo <tenjojeremy@gmail.com>
2021-10-10 18:28:47 -0400

    fixed default settings on not signed in

--- src/data/settings/useSettings/index.js
+++ src/data/settings/useSettings/index.js
@@ -3,15 +3,17 @@ import useCollection from '../../../lib/utils/data/useCollection'
 import defaultSettings from './defaultSettings'
 
 export default function useSettings() {
-  const collection = useCollection('settings', {
+  const settings = useCollection('settings', {
     onGet: (data) => {
       if (!data.length) {
-        collection.create.exec({ data: defaultSettings, disableSnackbar: true })
+        settings.create.exec({ data: defaultSettings, disableSnackbar: true })
       }
     },
   })
 
-  const [currentUserSettings] = collection.get.data || [defaultSettings]
+  const [currentUserSettings] = !settings.get.fetched
+    ? [defaultSettings]
+    : settings.get.data
 
-  return { ...collection, currentUserSettings }
+  return { ...settings, currentUserSettings }
 }
