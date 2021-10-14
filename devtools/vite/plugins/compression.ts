07113e8932f2ed5c396ff6e51ba7f5cd04ac54d3
Jeremy Tenjo <tenjojeremy@gmail.com>
2021-10-14 09:58:00 -0400

    docs

--- devtools/vite/plugins/compression.ts
+++ devtools/vite/plugins/compression.ts
@@ -2,6 +2,7 @@ import viteCompression from 'vite-plugin-compression'
 
 import { PayloadTypes } from '../config'
 
+// https://github.com/anncwb/vite-plugin-compression
 export default function viteCompressPlugin({}: PayloadTypes) {
   return viteCompression({
     algorithm: 'brotliCompress',
--- devtools/vite/plugins/react.ts
+++ devtools/vite/plugins/react.ts
@@ -1,3 +1,4 @@
 import viteReactPlugin from '@vitejs/plugin-react'
 
+// https://github.com/vitejs/vite/tree/main/packages/plugin-react
 export default viteReactPlugin
