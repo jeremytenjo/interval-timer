90006f13e33921b7f65a5d0d42cc59c7da9423a6
Jeremy Tenjo <tenjojeremy@gmail.com>
2021-10-19 13:05:27 -0400

    android plugin working

--- android/app/src/main/java/app/myintervaltimer/app/MainActivity.kt
+++ android/app/src/main/java/app/myintervaltimer/app/MainActivity.kt
@@ -6,7 +6,6 @@ import com.getcapacitor.BridgeActivity
 
 class MainActivity : BridgeActivity() {
     public override fun onCreate(savedInstanceState: Bundle?) {
-        println("hello from android!!!!!!!!!!!!!!!")
         super.onCreate(savedInstanceState)
         registerPlugin(CapacitorTimerNotification::class.java)
     }
