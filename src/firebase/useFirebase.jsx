897f4f4de1cbaca4c480972f2d87f31614d16099
Jeremy Tenjo <tenjojeremy@gmail.com>
2021-10-22 15:03:52 -0400

    c

--- src/firebase/useFirebase.jsx
+++ src/firebase/useFirebase.jsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext } from 'react'
+import { createContext, useContext } from 'react'
 import { getFirestore } from 'firebase/firestore'
 import { getAuth } from 'firebase/auth'
 import { initializeApp } from 'firebase/app'
