8c1b3b37a6843dbd832f1cfd300fa2020efef69c
Jeremy Tenjo <tenjojeremy@gmail.com>
2021-10-25 11:20:09 -0400

    includes sasets

--- devtools/vite/plugins/pwa/index.ts
+++ devtools/vite/plugins/pwa/index.ts
@@ -4,11 +4,13 @@ import { PayloadTypes } from '../../config'
 
 import getManifest from './manifest'
 import getWorkbox from './workbox'
+import getIncludeAssets from './includeAssets'
 
 // https://vite-plugin-pwa.netlify.app/guide/generate.html
 export default function vitePWAPlugin({ appConfig, isProdMode }: PayloadTypes) {
   return isProdMode
     ? VitePWA({
+        includeAssets: getIncludeAssets(),
         manifest: getManifest({ manifestJson: appConfig.manifestJson }),
         strategies: 'generateSW',
         workbox: getWorkbox(),
--- devtools/vite/plugins/pwa/workbox/index.ts
+++ devtools/vite/plugins/pwa/workbox/index.ts
@@ -2,7 +2,7 @@ import { GenerateSWOptions } from 'workbox-build'
 
 export default function getWorkbox(): GenerateSWOptions {
   return {
-    swDest: 'offline-sw',
+    // swDest: 'offline-sw',
     globIgnores: ['*.map*', '*.br*'],
     // for single page apps
     navigateFallback: '/index.html',
