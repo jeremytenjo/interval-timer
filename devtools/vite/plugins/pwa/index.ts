31d3a25386424fbf83e2610c524b04ebbfeef1b0
Jeremy Tenjo <tenjojeremy@gmail.com>
2021-10-25 11:16:38 -0400

    export

--- devtools/vite/plugins/pwa/index.ts
+++ devtools/vite/plugins/pwa/index.ts
@@ -3,12 +3,15 @@ import { VitePWA } from 'vite-plugin-pwa'
 import { PayloadTypes } from '../../config'
 
 import getManifest from './manifest'
+import getWorkbox from './workbox'
 
 // https://vite-plugin-pwa.netlify.app/guide/generate.html
 export default function vitePWAPlugin({ appConfig, isProdMode }: PayloadTypes) {
   return isProdMode
     ? VitePWA({
         manifest: getManifest({ manifestJson: appConfig.manifestJson }),
+        strategies: 'generateSW',
+        workbox: getWorkbox(),
       })
     : () => null
 }
---
+++ devtools/vite/plugins/pwa/workbox/index.ts
@@ -0,0 +1,12 @@
+import { GenerateSWOptions } from 'workbox-build'
+
+export default function getWorkbox(): GenerateSWOptions {
+  return {
+    swDest: 'offline-sw',
+    globIgnores: ['*.map*', '*.br*'],
+    // for single page apps
+    navigateFallback: '/index.html',
+    // exclude firebase namespace pages
+    navigateFallbackAllowlist: [/^(?!\/__).*/],
+  }
+}
