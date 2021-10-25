3c58e21f8ff97dee75e0a11d9be81033632d56e4
Jeremy Tenjo <tenjojeremy@gmail.com>
2021-10-25 10:03:26 -0400

    comment

--- android/app/src/main/java/app/myintervaltimer/app/plugins/CapacitorTimerNotification.kt
+++ android/app/src/main/java/app/myintervaltimer/app/plugins/CapacitorTimerNotification.kt
@@ -1,5 +1,6 @@
 package app.myintervaltimer.app.plugins
 
+import androidx.core.app.NotificationCompat
 import com.getcapacitor.JSObject
 import com.getcapacitor.Plugin
 import com.getcapacitor.PluginCall
@@ -11,6 +12,14 @@ class CapacitorTimerNotification : Plugin() {
     @PluginMethod
     fun echo(call: PluginCall) {
         println("hello from android!")
+
+//        var builder = NotificationCompat.Builder(this, 1)
+//            .setSmallIcon(R.drawable.notification_icon)
+//            .setContentTitle("My notification")
+//            .setContentText("Much longer text that cannot fit one line...")
+//            .setStyle(NotificationCompat.BigTextStyle()
+//                .bigText("Much longer text that cannot fit one line..."))
+//            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
         
         val value = call.getString("value")
         val result = JSObject()
