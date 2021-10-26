976ee66c6b8073822f2fc6d67add602f5a1bc8db
Jeremy Tenjo <tenjojeremy@gmail.com>
2021-10-26 15:27:10 -0400

    fixed notification

--- android/app/src/main/java/app/myintervaltimer/app/plugins/CapacitorTimerNotification.kt
+++ android/app/src/main/java/app/myintervaltimer/app/plugins/CapacitorTimerNotification.kt
@@ -1,9 +1,5 @@
 package app.myintervaltimer.app.plugins
 
-import androidx.core.app.NotificationCompat
-import androidx.core.app.NotificationManagerCompat
-import androidx.core.graphics.drawable.toBitmap
-import app.myintervaltimer.app.R
 import com.getcapacitor.JSObject
 import com.getcapacitor.Plugin
 import com.getcapacitor.PluginCall
@@ -15,7 +11,8 @@ class CapacitorTimerNotification : Plugin() {
     @PluginMethod
     fun showTimerNotification(call: PluginCall) {
         println("hello from android!")
-        sendNotification()
+
+        handleShowNotification(context)
 
         val value = call.getString("value")
         val result = JSObject()
@@ -27,27 +24,5 @@ class CapacitorTimerNotification : Plugin() {
     }
 
 
-    fun sendNotification() {
-        val CHANNEL_ID = context.getString(R.string.timer_notification_channel_id)
-        val notificationId = 101
-//        TODO fix small icon not showing
-        val smallIcon = R.drawable.logo_24x24
-        val title = "Title"
-        val text = "text"
-        val priority = NotificationCompat.PRIORITY_DEFAULT
-        val ongoing = true
-        val silent = true
 
-        var builder = NotificationCompat.Builder(context, CHANNEL_ID)
-            .setSmallIcon(smallIcon)
-            .setContentTitle(title)
-            .setContentText(text)
-            .setPriority(priority)
-            .setOngoing(ongoing)
-            .setSilent(silent)
-
-            with(NotificationManagerCompat.from(context)) {
-            notify(notificationId, builder.build())
-        }
-    }
 }
