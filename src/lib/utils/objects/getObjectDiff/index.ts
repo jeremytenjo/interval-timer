0251df03ce97c3eb720bad3ce191272c8007a45a
Jeremy Tenjo <tenjojeremy@gmail.com>
2021-11-14 10:53:37 -0500

    extraced areObjectsEqual

--- src/lib/utils/objects/getObjectDiff/index.ts
+++ src/lib/utils/objects/getObjectDiff/index.ts
@@ -1,20 +1,10 @@
-// https://www.samanthaming.com/tidbits/33-how-to-compare-2-objects/
-const getIsEqual = (obj1, obj2) => {
-  const obj1Sorted = Object.fromEntries(
-    Object.entries(obj1).sort(([, a]: any, [, b]: any) => a - b),
-  )
-  const obj2Sorted = Object.fromEntries(
-    Object.entries(obj2).sort(([, a]: any, [, b]: any) => a - b),
-  )
-  const isEqual = JSON.stringify(obj1Sorted) === JSON.stringify(obj2Sorted)
-  return isEqual
-}
+import areObjectsEqual from '../areObjectsEqual'
 
 export default function getObjectDiff(obj1, obj2) {
   const diff = Object.keys(obj1).reduce((result, key) => {
     if (!obj2.hasOwnProperty(key)) {
       result.push(key)
-    } else if (getIsEqual(obj1[key], obj2[key])) {
+    } else if (areObjectsEqual(obj1[key], obj2[key])) {
       const resultKeyIndex = result.indexOf(key)
       result.splice(resultKeyIndex, 1)
     }
