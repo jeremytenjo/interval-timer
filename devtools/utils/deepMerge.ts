060fa6415319a0d07707b047c8e47c03a8d19354
Jeremy Tenjo <tenjojeremy@gmail.com>
2021-11-14 10:50:05 -0500

    reduced getObjectDiff size by 5k

--- devtools/utils/deepMerge.ts
+++ devtools/utils/deepMerge.ts
@@ -4,6 +4,6 @@ import lodashMerge from 'lodash-es/merge'
  * @example
  * deepMerge(defaultConfigOptions, config)
  */
-export default function deepMerge(defaultValues, newValues) { 
-    return lodashMerge(defaultValues, newValues)
+export default function deepMerge(defaultValues, newValues) {
+  return lodashMerge(defaultValues, newValues)
 }
--- src/lib/utils/objects/getObjectDiff/index.ts
+++ src/lib/utils/objects/getObjectDiff/index.ts
@@ -1,10 +1,20 @@
-import isEqual from 'lodash-es/isEqual'
+// https://www.samanthaming.com/tidbits/33-how-to-compare-2-objects/
+const getIsEqual = (obj1, obj2) => {
+  const obj1Sorted = Object.fromEntries(
+    Object.entries(obj1).sort(([, a]: any, [, b]: any) => a - b),
+  )
+  const obj2Sorted = Object.fromEntries(
+    Object.entries(obj2).sort(([, a]: any, [, b]: any) => a - b),
+  )
+  const isEqual = JSON.stringify(obj1Sorted) === JSON.stringify(obj2Sorted)
+  return isEqual
+}
 
 export default function getObjectDiff(obj1, obj2) {
   const diff = Object.keys(obj1).reduce((result, key) => {
     if (!obj2.hasOwnProperty(key)) {
       result.push(key)
-    } else if (isEqual(obj1[key], obj2[key])) {
+    } else if (getIsEqual(obj1[key], obj2[key])) {
       const resultKeyIndex = result.indexOf(key)
       result.splice(resultKeyIndex, 1)
     }
