d1a619507dda35fd2643523745333fafe6d667eb
Jeremy Tenjo <tenjojeremy@gmail.com>
2021-11-13 19:48:41 -0500

    space

--- src/lib/components/Button/index.tsx
+++ src/lib/components/Button/index.tsx
@@ -3,6 +3,7 @@ import MUIButton, { ButtonProps } from '@mui/material/Button'
 type CustomProps = {
   text?: string
 }
+
 type Props = ButtonProps & CustomProps
 
 export default function Button({ text, children, ...rest }: Props) {
