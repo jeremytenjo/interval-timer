b502298d6adf414f8287cd170c6a02637e776ff8
Jeremy Tenjo <tenjojeremy@gmail.com>
2021-12-03 09:48:09 -0500

    added on update handlind

--- src/data/settings/useSettings/index.tsx
+++ src/data/settings/useSettings/index.tsx
@@ -1,10 +1,12 @@
 import useShowError from '../../../lib/components/feedback/useShowError'
+import useSnackBar from '../../../lib/components/Snackbar/useSnackbar'
 import useFirebaseCollection from '../../../lib/utils/data/useFirebaseCollection'
 
 import defaultSettings from './defaultSettings'
 
 export default function useSettings() {
   const showError = useShowError()
+  const snackbar = useSnackBar()
 
   const settings = useFirebaseCollection('settings', {
     returnDefaultData: true,
@@ -19,6 +21,23 @@ export default function useSettings() {
         error,
         message: `Error fetching settings, please try again.`,
       })
+    },
+    onUpdate: () => {
+      snackbar.show({ message: `Settings updated` })
+    },
+    onUpdateError: (error) => {
+      showError.show({
+        error,
+        message: `Error updating settings, please try again`,
+      })
+    },
+    onUpdateLoading: (loading) => {
+      if (loading) {
+        snackbar.show({
+          message: `Updating settings...`,
+          severity: 'info',
+        })
+      }
     },
   })
 
