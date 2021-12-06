d38f6ee3bbdeb72294e32b440ff6fee69c6a289d
Jeremy Tenjo <tenjojeremy@gmail.com>
2021-12-06 09:39:17 -0500

    enableauth

--- src/firebase/firebase.tsx
+++ src/firebase/firebase.tsx
@@ -20,6 +20,7 @@ export default function Firebase({ children }) {
       auth={auth}
       db={db}
       localStorageOptions={localStorageOptions}
+      enableAuth
     >
       {children}
     </FirebaseProvider>
