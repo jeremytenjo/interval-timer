3068d0fc6313d8290bbbb3207ff65451e48bd057
Jeremy Tenjo <tenjojeremy@gmail.com>
2022-03-07 22:06:55 -0500

    updated vite name

--- devtools/vite/scripts/startDevServer.ts
+++ devtools/vite/scripts/startDevServer.ts
@@ -1,4 +1,4 @@
 import shell from '../../nodeUtils/shell.js'
 ;(async function startDevServer() {
-  shell('vite --config ./devtools/vite/config.ts --host --port 3001')
+  shell('vite --config ./devtools/vite/vite.ts --host --port 3001')
 })()
